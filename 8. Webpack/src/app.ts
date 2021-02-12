4a5274de4dd57175b5957ea4c25e0bead02f8612
JuliWolf <for.julia96@gmail.com>
2021-02-12 21:27:30 +0300

    webpack setup

---
+++ 8. Webpack/src/app.ts
@@ -0,0 +1,10 @@
+import { ProjectInput } from '../src/components/project-input';
+import { ProjectList } from '../src/components/project-list';
+
+new ProjectInput();
+new ProjectList('active')
+new ProjectList('finished')
+
+
+
+
---
+++ 8. Webpack/src/models/drag-drop.ts
@@ -0,0 +1,15 @@
+// Drag & Drop Interfaces
+
+	export interface Draggable {
+		dragStartHandler(event: DragEvent): void;
+		dragEndHandler( event: DragEvent): void;
+	}
+	
+	export interface DrapTarget {
+		dragOverHandler(event: DragEvent): void;
+		dropHandler(event: DragEvent): void;
+		dragLeaveHandler(event: DragEvent): void;
+	}
+
+
+
