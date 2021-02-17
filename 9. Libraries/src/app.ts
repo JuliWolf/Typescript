942bd3022179ee42df20a709fd6d7fb5160dfefd
JuliWolf <for.julia96@gmail.com>
2021-02-17 13:02:59 +0300

    connect library

--- 9. Libraries/src/app.ts
+++ 9. Libraries/src/app.ts
@@ -1,3 +1,21 @@
+import "reflect-metadata";
+import { plainToClass } from 'class-transformer';
+import { Product } from './product.model';
 import _ from 'lodash';
 
 console.log(_.shuffle([1, 2, 3]));
+
+const products = [{ title: 'A carpet', price: 29.99 }, { title: 'A book', price: 10.99 }];
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(p1.getInformation());
+
+// const loadedProducts = products.map(prod => {
+// 	return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+	console.log(prod.getInformation());
+}
